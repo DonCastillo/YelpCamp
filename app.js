394f5f0ac5db75fbb28931a0c0817d67de87aac4
DonCastillo <don.qcastillo@yahoo.com>
2021-05-17 23:16:56 -0600

    add update function

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
+const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
 // db connection setting
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
 const db = mongoose.connection;
@@ -18,12 +20,12 @@ db.once('open', function() {
 })
 
 
-
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
+app.use(methodOverride('_method'))
 
 // application settings
 app.set('view engine', 'ejs')
@@ -41,15 +43,26 @@ app.get('/campgrounds', async (req, res) => {
 
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground)
-    // console.log(campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.patch('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+
 })
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const {id} = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', {campground})
+})
 
 app.get('/campgrounds/:id', async (req, res) => {
     const {id} = req.params
